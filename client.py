cb19cd8c8253b9caf415851c80261d261a6d96cb
KyleBonner <kylebonner99@gmail.com>
2024-06-20 16:45:19 -0700

    feat: add ip and port args

--- client.py
+++ client.py
@@ -2,15 +2,16 @@ import socket
 import sys
 
 try:    
-    client_type    = sys.argv[1]
-    client_subject = sys.argv[2]
-    client_message = sys.argv[3]
+    ip =   sys.argv[1]
+    port = sys.argv[2]
+    client_type    = sys.argv[3]
+    client_subject = sys.argv[4]
 except:
     print("Arguments Error")
     sys.exit()
 
 try:
-    if sys.argv[1] != "request" and sys.argv[1] != "reply":
+    if sys.argv[3] != "request" and sys.argv[3] != "reply":
         print("Invalid subject")
         sys.exit()
 except:
@@ -20,13 +21,15 @@ except:
     #Bug may occur here if the message arguments are not properly concatenated
 
 try:
-    message = ' '.join(sys.argv[3:])
+    message = ' '.join(sys.argv[5:])
 except:
     print("Message concatenation Error")
     sys.exit()
 
     
 print("This is a client")
+print("ip:", ip)
+print("port:", port)
 print("type:", client_type)
 print("subject:", client_subject)
 print("message:", message)
