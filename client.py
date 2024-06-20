06774dd2bae054649baeb44398c6c2b0592f6107
KyleBonner <kylebonner99@gmail.com>
2024-06-19 19:02:51 -0700

    feat: add type validation

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket
 import sys
 
-#VARIABLE
 try:    
     client_type  = sys.argv[1]
     client_subject = sys.argv[2]
@@ -10,9 +9,12 @@ except:
     print("Arguments Invalid")
     sys.exit()
 
+if sys.argv[1] != "request" and sys.argv[1] != "reply":
+    print("Invalid subject")
+    sys.exit()  
+
 
 print("This is a client")
 print("type:", sys.argv[1])
 print("subject:", sys.argv[2])
 print("message:", sys.argv[3])
-
